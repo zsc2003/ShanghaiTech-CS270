77d0cdf6a327dff4ca991a1c2c3cdba763caf765
zhoushch <zhoushch@shanghaitech.edu.cn>
2024-04-05 17:08:28 +0800

    update

--- homework/homework1/code/p1/p1a.py
+++ homework/homework1/code/p1/p1a.py
@@ -4,16 +4,9 @@ from p1_count_gray_num import count_gray_num
 origin_grain = plt.imread('./origin_images/grain.tif')
 gray_num = count_gray_num(origin_grain)
 
-
-fig, ax = plt.subplots(1, 2)
-        
-# show the origin image
-ax[0].imshow(origin_grain, cmap='gray')
-ax[0].set_title('origin image')
-
 # show the histogram of grain.tif
-ax[1].bar(range(256), gray_num)
-ax[1].set_xlabel('gray level')
-ax[1].set_ylabel('number of pixels')
-ax[1].set_title('histogram of grain.tif')
+plt.bar(range(256), gray_num)
+plt.xlabel('gray level')
+plt.ylabel('number of pixels')
+plt.title('histogram of grain.tif')
 plt.show()
