ebd848de0d1cf1e756858dba9cc29e6f790fcecf
zhoushch <zhoushch@shanghaitech.edu.cn>
2024-04-27 01:17:00 +0800

    finished p1a

--- homework/homework2/code/p1/p1a.py
+++ homework/homework2/code/p1/p1a.py
@@ -4,20 +4,45 @@ import matplotlib.pyplot as plt
 from p1_convolution import convolution
 from p1_normalization import normalization
 
+def show_res(origin_image, S_x_a, S_x_ab, S_y_b, S_y_ba):
+    fig, axs = plt.subplots(2, 3)
+    fig.subplots_adjust(hspace=0, wspace=0.1)
+
+    axs[0, 0].imshow(origin_image, cmap='gray')
+    axs[0, 0].set_title('origin image')
+    axs[0, 0].axis('off')
+
+    axs[0, 1].imshow(normalization(S_x_a), cmap='gray')
+    axs[0, 1].set_title('S_x_a')
+    axs[0, 1].axis('off')
+
+    axs[0, 2].imshow(normalization(S_x_ab), cmap='gray')
+    axs[0, 2].set_title('S_x_ab')
+    axs[0, 2].axis('off')
+
+    axs[1, 0].imshow(origin_image, cmap='gray')
+    axs[1, 0].set_title('origin image')
+    axs[1, 0].axis('off')
+
+    axs[1, 1].imshow(normalization(S_y_b), cmap='gray')
+    axs[1, 1].set_title('S_y_b')
+    axs[1, 1].axis('off')
+
+    axs[1, 2].imshow(normalization(S_y_ba), cmap='gray')
+    axs[1, 2].set_title('S_y_ba')
+    axs[1, 2].axis('off')
+    
+    plt.savefig('images/p1/p1a.png', dpi=300, bbox_inches='tight')
+
 origin_image = plt.imread('images/origin_images/Figure1.tif')
 
 a = np.array([[-1], [0], [1]])
-b = np.array([[1, 2, 1]])
+b = np.array([[1], [2], [1]])
 
-# directions = 
+S_x_a = convolution(origin_image, a)
+S_x_ab = convolution(S_x_a, b.T)
 
-# plt.imshow(origin_image)
-# gray image
-plt.imshow(origin_image, cmap='gray')
-plt.title('origin image')
-plt.axis('off')
-plt.show()
-# plt.savefig('images/p1/p1a.png', dpi=300, bbox_inches='tight')
+S_y_b = convolution(origin_image, b)
+S_y_ba = convolution(S_y_b, a.T)
 
-# origin image, S_x_a, S_x_ab
-# origin image, S_y_b, S_y_ba
+show_res(origin_image, S_x_a, S_x_ab, S_y_b, S_y_ba)
