9f789ac169e9dfa670ee87376d4f262881b10205
zhoushch <zhoushch@shanghaitech.edu.cn>
2024-04-27 22:56:41 +0800

    finished p1b

--- homework/homework2/code/p1/p1b.py
+++ homework/homework2/code/p1/p1b.py
@@ -1,14 +1,77 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from p1_normalization import normalization
 
-def Gaussian_high_pass_filter(size, D0):
-    filter = np.zeros((size, size))
-    for i in range(size):
-        for j in range(size):
-            filter[i, j] = 1 - np.exp(-((i - size // 2) ** 2 + (j - size // 2) ** 2) / (2 * D0 ** 2))
+def Gaussian_high_pass_filter(P, Q, D0):
+    filter = np.zeros((P, Q))
+    for i in range(P):
+        for j in range(Q):
+            filter[i, j] = 1 - np.exp(-((i - P // 2) ** 2 + (j - Q // 2) ** 2) / (2 * D0 ** 2))
     return filter
+
+def show_Gaussian(P, Q, H):
+    # show the Gaussian high pass filter
+    plt.figure()
+    plt.imshow(H, cmap='gray')
+    plt.title('Gaussian high pass filter')
+    plt.savefig('images/p1/p1b_Gaussian.png', dpi=300, bbox_inches='tight')
+
+    # show the gaussian high pass filter in 3D
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    x = np.arange(P)
+    y = np.arange(Q)
+    X, Y = np.meshgrid(x, y)
+    ax.plot_surface(X, Y, H, cmap='viridis')
+    plt.title('Gaussian high pass filter in 3D view')
+    plt.savefig('images/p1/p1b_Gaussian_3D.png', dpi=300, bbox_inches='tight')
+
+def show_filter(origin_image, origin_image_fft_shift, origin_image_fft_shift_filtered, origin_image_filtered):
+    fig, axs = plt.subplots(2, 2)
+    fig.subplots_adjust(hspace=0.4, wspace=0.0)
+
+    axs[0, 0].imshow(origin_image, cmap='gray')
+    axs[0, 0].set_title('origin image')
+
+    axs[0, 1].imshow(normalization(20 * np.log(1 + np.abs(origin_image_fft_shift))), cmap='gray')
+    axs[0, 1].set_title('origin image FFT')
+
+    axs[1, 0].imshow(normalization(20 * np.log(1 + np.abs(origin_image_fft_shift_filtered))), cmap='gray')
+    axs[1, 0].set_title('origin image FFT filtered')
+
+    axs[1, 1].imshow(normalization(origin_image_filtered), cmap='gray')
+    axs[1, 1].set_title('origin image filtered')
+
+    plt.savefig('images/p1/p1b_filter.png', dpi=300, bbox_inches='tight')
+
 
 origin_image = plt.imread('images/origin_images/Figure1.tif')
 
+w, h = origin_image.shape
 
+# power of 2 for FFT
+P = 2 ** np.ceil(np.log2(w)).astype(int)
+Q = 2 ** np.ceil(np.log2(h)).astype(int)
 
+# use Gaussian high pass filter to enhance the image
+D0 = 100
+H = Gaussian_high_pass_filter(P, Q, D0)
+
+show_Gaussian(P, Q, H)
+
+# FFT
+origin_image_fft = np.fft.fft2(origin_image, s=(P, Q))
+origin_image_fft_shift = np.fft.fftshift(origin_image_fft)
+
+# apply the Gaussian high pass filter
+origin_image_fft_shift_filtered = origin_image_fft_shift * H
+
+# IFFT
+origin_image_fft_filtered = np.fft.ifftshift(origin_image_fft_shift_filtered)
+origin_image_filtered = np.fft.ifft2(origin_image_fft_filtered)
+origin_image_filtered = np.real(origin_image_filtered)
+
+# crop the image
+origin_image_filtered = origin_image_filtered[:w, :h]
+
+show_filter(origin_image, origin_image_fft_shift, origin_image_fft_shift_filtered, origin_image_filtered)
--- homework/homework2/code/p2/p2a.py
+++ homework/homework2/code/p2/p2a.py
@@ -3,11 +3,12 @@ import matplotlib.pyplot as plt
 
 origin_image = plt.imread('images/origin_images/PET-scan.tif')
 
-plt.imshow(origin_image)
-plt.title('origin image')
-plt.axis('off')
 
 
-plt.show()
+# plt.imshow(origin_image)
+# plt.title('origin image')
+# plt.axis('off')
+
+# plt.show()
 # plt.savefig('images/p2/p2a.png', dpi=300, bbox_inches='tight')
 
